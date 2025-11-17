9a7d23bcd8136a74924a4e0e2ae60a01cb50a16e
DE\KAJ2BUE <justin.karam@de.bosch.com>
2025-11-17 15:14:02 +0100

    test

--- Betception-Backend/scripts/wait-for-db.js
+++ Betception-Backend/scripts/wait-for-db.js
@@ -22,6 +22,9 @@ async function checkConnection() {
     password,
     database,
     ssl: sslCa ? { ca: sslCa } : undefined,
+    sessionVariables: {
+      sql_require_primary_key: 0,
+    },
   });
   await connection.query('SELECT 1');
   await connection.end();
